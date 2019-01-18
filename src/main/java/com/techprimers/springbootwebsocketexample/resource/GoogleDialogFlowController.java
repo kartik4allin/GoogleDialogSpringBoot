d75d9d5aa12a690f62a7f565426a209033405b59
Kartik Ravichander <kartik.ravichander@Kartiks-MBP.attlocal.net>
2019-01-18 12:22:04 -0500

    2 commit

---
+++ src/main/java/com/techprimers/springbootwebsocketexample/resource/GoogleDialogFlowController.java
@@ -0,0 +1,26 @@
+package com.techprimers.springbootwebsocketexample.resource;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class GoogleDialogFlowController {
+
+  @Autowired
+  SimpMessagingTemplate template;
+
+
+   @RequestMapping(value = "/webhookSpringBoot")
+   public String receiveDatafromDialogflow() {
+    System.out.println("inside receiveDatafromDialogflow !!!!");
+     template.convertAndSend("/topic/user", new UserResponse("data from google dialog flow"));
+      return "Hello World";
+   }
+}

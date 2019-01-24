fee56874760662df5ac4834eabff760bc6ebd790
Kartik Ravichander <kartik.ravichander@Kartiks-MBP.attlocal.net>
2019-01-24 10:25:32 -0500

    5 commit

--- src/main/java/com/techprimers/springbootwebsocketexample/config/WebSocketConfig.java
+++ src/main/java/com/techprimers/springbootwebsocketexample/config/WebSocketConfig.java
@@ -5,14 +5,22 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-        stompEndpointRegistry.addEndpoint("/websocket-example");
+        //stompEndpointRegistry.addEndpoint("/websocket-example");
                 //.withSockJS();
+
+    RequestUpgradeStrategy upgradeStrategy = new TomcatRequestUpgradeStrategy();
+    stompEndpointRegistry.addEndpoint("/hello")
+    .setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy))
+    .setAllowedOrigins("*");
     }
 
     @Override
